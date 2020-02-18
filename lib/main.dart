3a6edfee8c7dc6dca7912fbf99ee96f359dec193
Isaí Arellano <isai.arellano@outlook.com>
2020-02-17 22:37:50 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mis_gastos/graph_widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mis Gastos',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _controller;
+  int currentPage = 9;
+
+
+ //Controlador para la paginación
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = PageController(
+      initialPage: currentPage,
+      viewportFraction: 0.4,
+    );
+  }
+
+
+ //Widget: Barra de menu abajo
+  Widget _bottomAction(IconData icon) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(icon),
+      ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8.0,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _bottomAction(FontAwesomeIcons.history),
+            _bottomAction(FontAwesomeIcons.chartPie),
+            SizedBox(width: 48.0),
+            _bottomAction(FontAwesomeIcons.wallet),
+            _bottomAction(Icons.settings),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: _body(),
+    );
+  }
+
+
+ //Widget: Cuerpo de la aplicación
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          _selector(),
+          _expenses(),
+          _graph(),
+          Container(
+            color: Colors.grey.withOpacity(0.15),
+            height: 8.0,
+          ),
+          _list(),
+        ],
+      ),
+    );
+  }
+
+ //Widget: para pasar de paginas para el selector de meses
+  Widget _pageItem(String name, int position) {
+    var _alignment;
+    final selected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.green,
+    );
+    final unselected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.grey.withOpacity(0.4),
+    );
+
+    if (position == currentPage) {
+      _alignment = Alignment.center;
+    } else if (position > currentPage) {
+      _alignment = Alignment.centerRight;
+    } else {
+      _alignment = Alignment.centerLeft;
+    }
+
+    return Align(
+      alignment: _alignment,
+      child: Text(name,
+        style: position == currentPage ? selected : unselected,
+      ),
+    );
+  }
+
+
+ //Widget: Selector de meses
+  Widget _selector() {
+    return SizedBox.fromSize(
+      size: Size.fromHeight(70.0),
+      child: PageView(
+        onPageChanged: (newPage) {
+          setState(() {
+            currentPage = newPage;
+          });
+        },
+        controller: _controller,
+        children: <Widget>[
+          _pageItem("Enero", 0),
+          _pageItem("Febrero", 1),
+          _pageItem("Marzo", 2),
+          _pageItem("Abril", 3),
+          _pageItem("Mayo", 4),
+          _pageItem("Junio", 5),
+          _pageItem("Julio", 6),
+          _pageItem("Agosto", 7),
+          _pageItem("Septiembre", 8),
+          _pageItem("Octubre", 9),
+          _pageItem("Noviembre", 10),
+          _pageItem("Diciembre", 11),
+        ],
+      ),
+    );
+  }
+
+//Widget: de gastos
+  Widget _expenses() {
+    return Column(
+      children: <Widget>[
+        Text("\$2361,41",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+          ),
+        ),
+        Text("Total de gastos",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+
+//Widget: grafico de gastos
+  Widget _graph() {
+    return Container(
+      height: 250.0,
+      child: GraphWidget(),
+    );
+  }
+
+//Widget: Items para listarlos
+  Widget _item(IconData icon, String name, int percent, double value) {
+    return ListTile(
+      leading: Icon(icon, size: 32.0,),
+      title: Text(name,
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0
+        ),
+      ),
+      subtitle: Text("$percent% de gastos",
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Colors.grey,
+        ),
+      ),
+      trailing: Container(
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("\$$value",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+//Widget: para listar items
+  Widget _list() {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) =>
+            _item(FontAwesomeIcons.shoppingCart, "Despensa", 14, 145.12,),
+        separatorBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.grey.withOpacity(0.15),
+            height: 8.0,
+          );
+        },
+      ),
+    );
+  }
+}
